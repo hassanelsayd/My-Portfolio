dd1f0b632303999354afdfefbf5e21736d09847a
Hassan Elsayd <hassan22elsayd@gmail.com>
2024-06-03 02:10:00 +0300

    My portfolio

---
+++ src/Details.js
@@ -0,0 +1,169 @@
+// Enter all your detials in this file
+
+// Profile Image
+import profile from "./assets/profile.png";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import nextjs from "./assets/techstack/nextjs.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import mui from "./assets/techstack/mui.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.png";
+import projectImage2 from "./assets/projects/project2.png";
+import projectImage3 from "./assets/projects/project3.png";
+import projectImage4 from "./assets/projects/project4.png";
+import projectImage5 from "./assets/projects/project5.png";
+import projectImage6 from "./assets/projects/project6.png";
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Hassan Elsayd",
+  tagline: "React Frontend web Developer",
+  img: profile,
+  about: `Experienced Frontend Developer proficient in building responsive, 
+  high-performance web applications with ReactJS and NextJS. 
+  Skilled in creating clean, maintainable code and modern design 
+  principles to enhance user experiences. Collaborative team player 
+  with a passion for leveraging cutting-edge technologies to innovate 
+  and solve complex problems.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/hassan-elsayd-218a9510a/",
+  github: "https://github.com/hassanelsayd",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Frontend Web Developer",
+    Company: `Elite (Military Service Place)`,
+    Location: "Egypt",
+    Type: "Full Time",
+    Duration: "May 2023 - May 2024",
+  },
+  {
+    Position: " Software Development internship",
+    Company: `ITI`,
+    Location: "Egypt, Ismailia",
+    Type: "Internship",
+    Duration: "July 2021 - August 2021",
+  },
+  {
+    Position: "Full-stack internship",
+    Company: `FCI`,
+    Location: "Egypt, Ismailia",
+    Type: "Internship",
+    Duration: "July 2020 - August 2020",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Bachelor of Computer Science",
+    Company: "Faculty of Computers and Informatics Suez Canal University",
+    Location: "Onsite",
+    Type: "Bachelor",
+    Duration: "2018 - 2022",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  mui: mui,
+  nextjs: nextjs,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Salehunter",
+    image: projectImage1,
+    description: `Developing a search engine across Android, Desktop, and web platforms to compare product prices 
+    from e-commerce giants like Amazon and Jumia, alongside registered stores hosted on our 
+    platform. Enhancing user experience with real-time pricing, availability, and nearby store 
+    information`,
+    techstack: "Reactjs, Javascript,Redux, Mui",
+    previewLink: "https://sale-hunter.vercel.app/",
+    githubLink: "https://github.com/hassanelsayd/SaleHunter",
+  },
+  {
+    title: "IReels",
+    image: projectImage2,
+    description: `A TikTok clone developed using Next.js, featuring a responsive and dynamic interface. Includes 
+    functionalities such as video uploading, liking, commenting, and user profiles. Designed to deliver 
+    a seamless user experience similar to TikTok's popular app.
+    `,
+    techstack:
+      "Reactjs(Nextjs) – Tailwind – Sanity (CMS) – Zustand & Redux - Typescript",
+    previewLink: "https://tiktok-clone-final-two.vercel.app/",
+    githubLink: "https://github.com/hassanelsayd/tiktok_clone",
+  },
+  {
+    title: "Plants",
+    image: projectImage3,
+    description: `This React.js landing page for our plant store promotes environmental health by offering a diverse selection of eco-friendly plants. Enhance your space and contribute to a greener planet with every purchase.`,
+    techstack: "Reactjs - Javascript - css",
+    previewLink: "https://plants-gules.vercel.app/",
+    githubLink: "https://github.com/hassanelsayd",
+  },
+  {
+    title: "Elite Beach",
+    image: projectImage4,
+    description: `This React.js landing page for Elite Beach Village showcases our luxurious services and premier accommodation units. Discover your perfect beachfront getaway with our seamless, user-friendly interface.`,
+    techstack: "Reactjs - Javascript - css",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Booksy",
+    image: projectImage5,
+    description: `Developed a Books Library application using ReactJS to browse and manage a collection of books 
+    sourced from an online API. Implemented features to fetch and display book data In different types.
+    `,
+    techstack: "React, Typescript, RestAPI",
+    previewLink: "https://booksy-two.vercel.app/",
+    githubLink: "https://github.com/hassanelsayd/booksy",
+  },
+  {
+    title: "Ideahub",
+    image: projectImage6,
+    description: `Developed a Blogs App using ReactJS and Next.js, featuring blog post display, user 
+    authentication with Auth.js, post creation using React Quill, and individual post browsing.`,
+    techstack: "Nextjs – Auth.js – React Quill – JSON-SERVER - CSS Modules",
+    previewLink: "https://vercel.com/hassan-elsayds-projects/ideahub",
+    githubLink: "https://github.com/hassanelsayd/ideahub",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "hassan22elsayd@gmail.com",
+  phone: "+201033704442",
+};
